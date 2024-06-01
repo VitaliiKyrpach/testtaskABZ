26d046c6cfa6c046bf33929375098c9021ddbedd
VitaliiKyrpach <126057381+VitaliiKyrpach@users.noreply.github.com>
2024-06-01 19:52:00 +0300

    add lazy, sqoosh img

--- src/components/CatalogCard/CatalogCard.jsx
+++ src/components/CatalogCard/CatalogCard.jsx
@@ -1,16 +1,23 @@
 import svg from "../../img/sprite.svg";
+import { useInView } from "react-intersection-observer";
 
 export const CatalogCard = ({ user }) => {
   const { name, position, email, phone, photo } = user;
+  const { ref, inView } = useInView({
+    threshold: 0.2,
+    triggerOnce: true,
+  });
+
   return (
-    <li className="card">
-      {photo.includes("placeholder") ? (
-        <svg className="photo">
-          <use href={`${svg}#icon-photo-cover`}></use>
-        </svg>
-      ) : (
-        <img className="photo" src={photo} alt="avatar" />
-      )}
+    <li className="card" ref={ref}>
+      {inView &&
+        (photo.includes("placeholder") ? (
+          <svg className="photo">
+            <use href={`${svg}#icon-photo-cover`}></use>
+          </svg>
+        ) : (
+          <img className="photo" src={photo} alt="avatar" />
+        ))}
       <h3 className="name">{name}</h3>
       <p className="text">{position}</p>
       <p className="text">{email}</p>
